3258957fdbb18b7005cc280de9b85ea58fb752bc
comppy <kim1122aass@gmail.com>
2026-05-04 16:47:49 +0900

    아이콘 추가

--- app/root.tsx
+++ app/root.tsx
@@ -25,10 +25,11 @@ export const links: Route.LinksFunction = () => [
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" type="image/x-icon" href="/abigail_sit_NBG_2.png" />
         <Meta />
         <Links />
       </head>
